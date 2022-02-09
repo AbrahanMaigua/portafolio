773473e76487168bc5419ad9af0bbb5074684d54
leivakuro765 <abrahanamaigua15@gmail.com>
2022-02-09 14:34:15 -0300

    adatar plantilla para utilizar en django

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "hello",
+    "home",
+
 ]
 
 MIDDLEWARE = [
@@ -57,7 +59,8 @@ ROOT_URLCONF = "gettingstarted.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+        "/app/"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
